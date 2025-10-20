61d9273ac0ac68b8d1948b1275cd6ae4bb8fd6f3
김예안 <kjan0929@naver.com>
2025-10-20 18:01:40 +0900

    feat(app): 문자열 덧셈 계산기 메인 로직 구현
    App 클래스의 run 메서드의 메인 실행 흐름을 구현합니다.
    기존에 구현한 모듈들을 조합하여 다음 순서로 로직을 실행합니다.
    
    - 사용자 문자열 입력 (Console.readLineAsync)
    - 입력값 유효성 검사 및 구분자  치환(validateSeperator)
    - 기본 구분자(,) 기준으로 분리 및 필터링
    - 숫자 합계 계산 (calculateSum)
    - 최종 결과 출력 (Console.print)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import { Console } from "@woowacourse/mission-utils";
+import validateSeperator from "./inputProcessor.js";
+import calculateSum from "./calculateSum.js";
 
 class App {
   async run() {
     // 문자열 입력 받기
     const input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.");
+
+    // 입력받은 문자열을 ','로 구분자가 통일된 문자열로 변환
+    // 유효하지 않은 문자열 예외 처리
+    const convertedInput = validateSeperator(input);
+
+    // 기본 구분자 기준으로 각 숫자 추출 및 합 계산
+    const filteredNumbers = convertedInput.split(",").filter(Number);
+    const result = calculateSum(filteredNumbers);
+
+    Console.print(`결과 : ${result}`);
   }
 }
 
