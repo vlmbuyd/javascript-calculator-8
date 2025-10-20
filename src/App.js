9545e774750e8c6e8e22aed4999a4a40d99a5080
김예안 <kjan0929@naver.com>
2025-10-20 17:32:44 +0900

    feat: 사용자 문자열 입력 기능 구현
    - @woowacourse/mission-utils에서 제공하는 Console API를 사용하여 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    // 문자열 입력 받기
+    const input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.");
+  }
 }
 
 export default App;
