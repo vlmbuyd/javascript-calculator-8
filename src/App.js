4b12125ffe60d2c7f7ff2634ffbefae38afe244b
김예안 <kjan0929@naver.com>
2025-10-20 22:13:41 +0900

    refactor(app): 문자열 매직 스트링 상수화

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import { Console } from "@woowacourse/mission-utils";
 import validateSeperator from "./inputProcessor.js";
 import calculateSum from "./calculateSum.js";
+import { DEFAULT_SEPERATOR, IO_MESSAGE } from "./constants.js";
 
 class App {
   async run() {
     // 문자열 입력 받기
-    const input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.");
+    const input = await Console.readLineAsync(IO_MESSAGE.INPUT_PROMPT);
 
     // 입력받은 문자열을 ','로 구분자가 통일된 문자열로 변환
     // 유효하지 않은 문자열 예외 처리
     const convertedInput = validateSeperator(input);
 
     // 기본 구분자 기준으로 각 숫자 추출 및 합 계산
-    const filteredNumbers = convertedInput.split(",").filter(Number);
+    const filteredNumbers = convertedInput
+      .split(DEFAULT_SEPERATOR.COMMA)
+      .filter(Number);
     const result = calculateSum(filteredNumbers);
 
-    Console.print(`결과 : ${result}`);
+    Console.print(IO_MESSAGE.OUTPUT_PROMPT(result));
   }
 }
 
---
+++ src/constants.js
@@ -0,0 +1,20 @@
+export const DEFAULT_SEPERATOR = {
+  COMMA: ",",
+  COLON: ":",
+};
+
+export const CUSTOM_SEPERATOR = {
+  START: "//",
+  END: "\\n",
+};
+
+export const IO_MESSAGE = {
+  INPUT_PROMPT: "덧셈할 문자열을 입력해 주세요.\n",
+  OUTPUT_PROMPT: (result) => `결과 : ${result}`,
+};
+
+export const ERROR_MESSAGE = {
+  INVALID_CUSTOM_SEPERATOR:
+    '[ERROR] 커스텀 구분자 선언이 잘못되었습니다. \n 올바른 형식은 "//<구분자>\\n"입니다.',
+  INVALID_SEPERATOR: "[ERROR] 유효하지 않은 구분자가 포함되어 있습니다.",
+};
