af37af1356c2967191be6db418fe5b3f7eb31120
김예안 <kjan0929@naver.com>
2025-10-20 22:52:26 +0900

    fix(validate): 음수 입력 시의 오류 예외 처리 추가
    기존 유효성 검사 로직에서는 음수(-)를 허용하고 있었습니다.
    숫자 입력 값은 '양수'만 허용되는 요구사항에 따라 별도의 검증 로직을 추가합니다.
    
    - 마이너스 기호('-')를 발견하면,`INVALID_NUMBER` 에러를 발생시키도록 유효성 로직 추가
    - `isNaN` 검사 로직에서 음수 허용 로직 제거
    
    이로써 음수 입력을 검증하는 "예외 테스트" 케이스를 통과시킵니다.

--- src/constants.js
+++ src/constants.js
@@ -21,4 +21,5 @@ export const ERROR_MESSAGE = {
   INVALID_CUSTOM_SEPERATOR:
     '[ERROR] 커스텀 구분자 선언이 잘못되었습니다. \n 올바른 형식은 "//<구분자>\\n"입니다.',
   INVALID_SEPERATOR: "[ERROR] 유효하지 않은 구분자가 포함되어 있습니다.",
+  INVALID_NUMBER: "[ERROR] 음수는 허용되지 않습니다.",
 };
