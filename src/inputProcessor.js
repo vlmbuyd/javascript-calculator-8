96d094d678c253c98aecd94a1beb0f8d84676880
김예안 <kjan0929@naver.com>
2025-10-20 17:52:36 +0900

    feat(validate): 구분자 유효성 검사 로직 추가
    - 유효성 검사 로직을 `validateSeperator` 함수로 분리
    - 커스텀 구분자 선언 형식(`//...\\n`) 유효성 검사 추가
    - 숫자와 기본 구분자(,) 외의 문자가 포함된 경우 예외 처리

--- src/inputProcessor.js
+++ src/inputProcessor.js
@@ -1,3 +1,5 @@
+const basicSeperator = new Set([",", ":"]); // 기본 구분자 리스트
+
 /**
  * 입력된 문자열의 모든 구분자를 기본 구분자(,)로 치환하는 함수
  *
@@ -38,8 +40,39 @@ const convertToBasicSeperator = (input) => {
 
       customSeperatorIdxList = []; // 인덱스 리스트 초기화
     }
+
+    // [ERROR] 커스텀 구분자의 종료 문자열(\n)이 유효하지 않은 경우 예외 처리
+    if (idx === input.length - 1 && customSeperatorIdxList.length === 1) {
+      throw new Error(
+        `[ERROR] 커스텀 구분자 선언이 잘못되었습니다. \n 올바른 형식은 "//<구분자>\\n"입니다.`
+      );
+    }
   });
 
   // 모든 구분자를 ','로 통일
   return convertedInput.replaceAll(":", ",");
 };
+
+/**
+ * 유효한 문자열인지 검증하고 유효성에 따라 값을 반환하는 함수
+ * - 유효한 문자열이면 모든 구분자를 기본 구분자로 치환하여 반환
+ * - 유효하지 문자열이면 에러 메시지를 출력하고 프로그램 종료
+ *
+ * @param {string} input - 사용자 입력 문자열
+ * @returns {string} - 모든 구분자가 기본 구분자(,)로 치환된 문자열 (e.g. "1,2,,3")
+ */
+const validateSeperator = (input) => {
+  // 모든 구분자를 기본 구분자(,)로 치환
+  const convertedInput = convertToBasicSeperator(input);
+
+  // [ERROR] 유효하지 않은 구분자 예외 처리
+  [...convertedInput].forEach((item) => {
+    if (!Number(item) && !basicSeperator.has(item)) {
+      throw new Error(`[ERROR] 유효하지 않은 구분자가 포함되어 있습니다.`);
+    }
+  });
+
+  return convertedInput;
+};
+
+export default validateSeperator;
